f6f7f81c642b011f6d640ed86b33b4557b9e7b52
itmonksakshay <akshay.bhatt@hotmail.com>
2024-03-07 09:32:05 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  // Object
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const light = new THREE.AmbientLight( 0xffffff ); 
+  scene.add( light );
+
+  scene.add(camera)
+
+
+  React.useEffect(() => {
+
+
+    // Canvas
+    const canvas = document.querySelector('canvas.webgl')
+    
+
+    // ...
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas
+    })
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render(scene, camera)
+
+    return () => {
+      // myElement.removeEventListener('load');
+    }
+
+
+  }, [camera, scene])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas className="webgl"></canvas>
   );
 }
 
